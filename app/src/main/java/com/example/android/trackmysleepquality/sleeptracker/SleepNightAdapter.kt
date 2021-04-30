1347bdb5f440f0007b4809a9629cd967a02ce3d7
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-04-30 10:28:27 +0100

    Implemented DiffUtil and changed RecyclerView Adapter to List Adapter

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -21,30 +21,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.convertDurationToFormatted
 import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
+// TODO (03) Update SleepNightAdapter class to extend ListAdapter. //DONE
+class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
-    // TODO (03) Update SleepNightAdapter class to extend ListAdapter.
 
-    // TODO (04) Delete the data field and getItemCount() function.
 
-    var data = listOf<SleepNight>()
+    // TODO (04) Delete the data field and getItemCount() function. //DONE
+    /*var data = listOf<SleepNight>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    override fun getItemCount() = data.size
+    override fun getItemCount() = data.size*/
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        // TODO (05) Replace data[position] with getItem().
-        val item = data[position]
+        // TODO (05) Replace data[position] with getItem(). //DONE
+        val item = getItem(position)
 
         holder.bind(item)
     }
@@ -85,8 +87,20 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
         }
     }
 
-    // TODO (01) Create a new class called SleepNightDiffCallback that extends
+    // TODO (01) Create a new class called SleepNightDiffCallback that extends //DONE
     // DiffUtil.ItemCallback<SleepNight>.
+    class SleepNightDiffCallback: DiffUtil.ItemCallback<SleepNight>() {
+        // TODO (02) In SleepNightDiffCallback, override areItemsTheSame() and areContentsTheSame(). //DONE
 
-    // TODO (02) In SleepNightDiffCallback, override areItemsTheSame() and areContentsTheSame().
+        override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            return oldItem.nightId == newItem.nightId
+        }
+
+        override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+
 }
